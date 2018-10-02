c24a9c2f84e3b5cc8d065526e9de6dcbaca3f850
Kevin Chen <xkevinchen@gmail.com>
2018-10-02 17:16:51 -0400

    Display TV show data from API

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+let webpack = require('webpack');
+let path = require('path');
+
 module.exports = {
   entry: "./lib/main.js",
   output: {
     path: __dirname,
-    filename: "./lib/js_domination.js"
+    filename: "lib/js_domination.js"
 	},
 	devtool: "source-map"
 };
